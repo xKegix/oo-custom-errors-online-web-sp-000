4950be3d2770df15b00f4924ccbe17096bc3fd69
Keginold Philip <keginoldp@hotmail.com>
2021-04-01 15:51:48 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,8 +7,12 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
   end
+  
   class PartnerError < StandardError
   end
 end
